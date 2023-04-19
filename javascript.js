1e769153c768d4577969a40ef02eeb92e9191f22
bernardouribe98 <127761873+bernardouribe98@users.noreply.github.com>
2023-04-19 12:20:06 -0500

    Create function to play rock-paper-scissors rounds

--- javascript.js
+++ javascript.js
@@ -16,8 +16,39 @@ function getComputerChoice () {
 }
 
 function playRound (playerSelection, computerSelection) {
+    playerSelection = playerSelection.toString()
+    computerSelection = computerSelection.toString()
     playerSelection = playerSelection.toLowerCase()
     computerSelection = computerSelection.toLowerCase()
-    return playerSelection, computerSelection
+    if ((playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissors") && (computerSelection === "rock" || computerSelection === "paper" || computerSelection === "scissors")){
+        if (playerSelection === computerSelection){
+            result = "There is a draw!"
+        }
+        else {
+            if (playerSelection === "rock" && computerSelection === "paper"){
+                result = "You lost! Paper beats rock."
+            }
+            else if (playerSelection === "rock" && computerSelection === "scissors"){
+                result = "You won! Rock beats scissors."
+            }
+            else if (playerSelection === "paper" && computerSelection === "rock"){
+                result = "You won! Paper beats rock."
+            }
+            else if (playerSelection === "paper" && computerSelection === "scissors"){
+                result = "You lost! Scissors beats paper."
+            }
+            else if (playerSelection === "scissors" && computerSelection === "rock"){
+                result = "You lost Rock beats scissors."
+            }
+            else if (playerSelection === "scissors" && computerSelection === "paper"){
+                result = "You won! Scissors beats paper."
+            }
+        }
+    }
+    else {
+        result = "not OK"
+    }
+    return {playerSelection, computerSelection, result}
+
 
 }
